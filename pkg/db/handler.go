b9a9057f0c6cc5b2e31dc495f7a025f9ab1a76bc
sittipak <sittipak.ssw@gmail.com>
2024-04-25 21:57:21 +0700

    Updated tax logic

--- pkg/db/handler.go
+++ pkg/db/handler.go
@@ -5,7 +5,7 @@ import (
 	"encoding/csv"
     "strconv"
     "strings"
-
+	
 	"github.com/labstack/echo/v4"
 )
 
@@ -16,7 +16,7 @@ func CalculateTaxHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid request payload")
 	}
 
-	tax, taxLevels := calculateTax(req.TotalIncome, req.WHT, req.Allowances)
+	tax, taxLevels, tax_wht:= calculateTax(req.TotalIncome, req.WHT, req.Allowances)
 
 	var convertedTaxLevels []map[string]interface{}
 	for _, level := range taxLevels {
@@ -28,7 +28,7 @@ func CalculateTaxHandler(c echo.Context) error {
 	}
 
 	var taxRefund float64
-	taxRefund = calculateTaxRefund(tax, req.WHT)
+	taxRefund = calculateTaxRefund(tax_wht, req.WHT)
 
 	res := map[string]interface{}{
 		"tax":      tax,
@@ -92,10 +92,12 @@ func CalculateTaxFromCSVHandler(c echo.Context) error {
 			continue
 		}
 
-		tax, _ := calculateTax(totalIncome, wht, []Allowance{{AllowanceType: "donation", Amount: donation}})
+		tax, _, taxRefund := calculateTax(totalIncome, wht, []Allowance{{AllowanceType: "donation", Amount: donation}})
+
 		taxData := map[string]interface{}{
 			"totalIncome": totalIncome,
 			"tax":         tax,
+			"taxRefund":   taxRefund,
 		}
 		taxes = append(taxes, taxData)
 	}
--- pkg/db/service.go
+++ pkg/db/service.go
@@ -1,13 +1,7 @@
 package db
 
 import (
-	"encoding/csv"
 	"math"
-	"net/http"
-	"strconv"
-	"strings"
-
-	"github.com/labstack/echo/v4"
 )
 
 type Allowance struct {
@@ -19,10 +13,10 @@ type taxRequest struct {
 	TotalIncome       float64     `json:"totalIncome"`
 	WHT               float64     `json:"wht"`
 	Allowances        []Allowance `json:"allowances"`
-	PersonalDeduction float64     `json:"personalDeduction"`
 }
+var personalDeduction float64 = 60000.0
 
-func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (float64, []map[string]interface{}) {
+func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (float64, []map[string]interface{}, float64) {
 
 	var totalDeductions float64
 	for _, allowance := range allowances {
@@ -32,26 +26,12 @@ func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (flo
 			} else {
 				totalDeductions += allowance.Amount
 			}
-		} else if allowance.AllowanceType == "personal" {
-			if allowance.Amount < 10000.0 {
-				totalDeductions += 10000.0
-			} else {
-				totalDeductions += allowance.Amount
-			}
-		} else if allowance.AllowanceType == "k-receipt" {
-			if allowance.Amount > 50000.0 {
-				totalDeductions += 50000.0
-			} else {
-				totalDeductions += allowance.Amount
-			}
-		}
+		} 
 	}
 
-	totalDeductions += 60000.0
 
-	taxableIncome := totalIncome - totalDeductions
+	taxableIncome := totalIncome - totalDeductions - personalDeduction
 
-	// Calculate tax based on tax brackets
 	taxLevels := []map[string]interface{}{
 		{"level": "0-150,000", "tax": 0.0},
 		{"level": "150,001-500,000", "tax": 0.0},
@@ -61,6 +41,7 @@ func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (flo
 	}
 
 	var tax float64
+	var taxRefund float64
 
 	switch {
 	case taxableIncome <= 150000.0:
@@ -70,6 +51,7 @@ func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (flo
 
 	case taxableIncome <= 500000.0:
 		tax = (taxableIncome - 150000.0) * 0.1
+		taxRefund = calculateTaxRefund(tax, wht)
 		if wht > 0 {
 			tax -= wht
 		}
@@ -77,31 +59,36 @@ func calculateTax(totalIncome float64, wht float64, allowances []Allowance) (flo
 
 	case taxableIncome <= 1000000.0:
 		tax = 35000.0 + ((taxableIncome - 500000.0) * 0.15)
+		taxRefund = calculateTaxRefund(tax, wht)
 		if wht > 0 {
 			tax -= wht
 		}
 		taxLevels[2]["tax"] = math.Round(tax)
 
 	case taxableIncome <= 2000000.0:
-		tax = 75000.0 + (taxableIncome-1000000.0)*0.2
+		tax = 75000.0 + ((taxableIncome-1000000.0)*0.2)
+		taxRefund = calculateTaxRefund(tax, wht)
 		if wht > 0 {
 			tax -= wht
 		}
 		taxLevels[3]["tax"] = math.Round(tax)
 
 	default:
-		tax = 175000 + (taxableIncome-2000000)*0.35
+		tax = 175000 + ((taxableIncome-2000000)*0.35)
+		taxRefund = calculateTaxRefund(tax, wht)
 		if wht > 0 {
 			tax -= wht
 		}
+		
 		taxLevels[4]["tax"] = math.Round(tax)
+		
 	}
 
 	if tax < 0.0 {
 		tax = 0.0
 	}
 
-	return math.Round(tax), taxLevels
+	return math.Round(tax), taxLevels, math.Round(taxRefund)
 }
 
 func calculateTaxRefund(tax float64, wht float64) float64 {
@@ -114,53 +101,3 @@ func calculateTaxRefund(tax float64, wht float64) float64 {
 	return math.Round(taxRefund)
 }
 
-func calculateTaxFromCSVHandler(c echo.Context) error {
-	file, err := c.FormFile("taxFile")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "failed to get CSV file")
-	}
-
-	src, err := file.Open()
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "failed to open CSV file")
-	}
-	defer src.Close()
-
-	reader := csv.NewReader(src)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "failed to read CSV file")
-	}
-
-	var taxes []map[string]interface{}
-	for _, record := range records {
-
-		totalIncome, err := strconv.ParseFloat(strings.TrimSpace(record[0]), 64)
-		if err != nil {
-			continue
-		}
-
-		wht, err := strconv.ParseFloat(strings.TrimSpace(record[1]), 64)
-		if err != nil {
-			continue
-		}
-
-		donation, err := strconv.ParseFloat(strings.TrimSpace(record[2]), 64)
-		if err != nil {
-			continue
-		}
-
-		tax, _ := calculateTax(totalIncome, wht, []Allowance{{AllowanceType: "donation", Amount: donation}})
-		taxData := map[string]interface{}{
-			"totalIncome": totalIncome,
-			"tax":         tax,
-		}
-		taxes = append(taxes, taxData)
-	}
-
-	res := map[string]interface{}{
-		"taxes": taxes,
-	}
-
-	return c.JSON(http.StatusOK, res)
-}
